edf94bc54a14f3077ec4103a3ab93a2402da191a
James Shufelt <jamesshufeltiii@gmail.com>
2026-08-05 02:15:29 -0500

    stuff

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   output: "export",
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
+  basePath: "/nextjstemtemplate", // Add this line
   allowedDevOrigins: ["0.0.0.0"],
 };
 
-export default nextConfig;
