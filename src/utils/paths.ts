fbdeacbc245a0465e8dc5b4c3c23eb9c01aea5e7
James Shufelt <28380000+peterhuzza@users.noreply.github.com>
2026-08-07 14:25:54 -0500

    testing

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -2,7 +2,7 @@ export const getPublicPath = (path: string) => {
   const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
   // 1. If the path is already an absolute URL (starts with http), return it
-  if (path.startsWith("http")) return path;
+  if (path.startsWith("https")) return path;
 
   // 2. If the path is already absolute for the current domain (starts with /)
   if (path.startsWith("/")) {
